aeb31455ad5caf393eec91c95055620cf8738a8a
Vivek Badan <vivek.badan@gmail.com>
2025-11-01 10:44:35 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (52.48%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -2,13 +2,47 @@ class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
         int n=nums.size();
+        int start=0;
+        int last= n -1;
+        
         vector<int>ans(2,-1);
-        for(int i=0;i<n;i++){
-            if(target==nums[i]){
-                if(ans[0]==-1)ans[0]=i;
-                ans[1]=i;
+
+        //First occurence
+
+        while(start<=last){
+            int mid = start+(last-start)/2 ;
+            if(target==nums[mid]){
+                
+                ans[0]=mid;
+                last=mid-1;
+                
+            }
+            else if(target>nums[mid]){
+                start=mid+1;
+            }
+            else{
+                last=mid-1;
             }
         }
+        //second occurence 
+    start = 0;
+    last = n - 1;   
+    
+        while(start<=last){
+            int mid = start+(last-start)/2 ;
+            if(target==nums[mid]){
+                
+                ans[1]=mid;
+               
+                start=mid+1;            }
+            else if(target>nums[mid]){
+                start=mid+1;
+            }
+            else{
+                last=mid-1;
+            }
+        }
+        
         return ans;
         
     }
