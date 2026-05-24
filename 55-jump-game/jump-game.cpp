543a96a6169ec82c333945b2122142e8f56c242b
Vivek Badan <vivek.badan@gmail.com>
2026-05-24 09:50:14 +0530

    Time: 0 ms (100.00%) | Memory: 52.4 MB (56.74%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int maxind=0;
-        int n =nums.size();
-        for(int i=0;i<n-1;i++){
-            if(i>maxind)return false;
-            maxind=max(maxind,i+nums[i]);
+
+        int maxind = 0;
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+
+            if (i > maxind)
+                return false;
+
+            maxind = max(maxind, i + nums[i]);
+
+            if (maxind >= n - 1)
+                return true;
         }
-        return maxind >= n - 1;;
-        
+
+        return true;
     }
 };
