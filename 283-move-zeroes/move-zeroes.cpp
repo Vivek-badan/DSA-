f06621ce898e1f3abe2fc31be5d54a1883581f5f
Vivek Badan <vivek.badan@gmail.com>
2026-05-26 18:36:47 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (54.79%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -2,13 +2,34 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
 
-        int j = 0;
+        // Pointer to first zero
+        int j = -1;
 
-        for(int i = 0; i < nums.size(); i++) {
+        // Find first zero
+        for (int i = 0; i < nums.size(); i++) {
 
-            if(nums[i] != 0) {
+            if (nums[i] == 0) {
+                j = i;
+                break;
+            }
+        }
+
+        // If no zero exists
+        if (j == -1) return;
+
+        // Rearrange
+        for (int i = j + 1; i < nums.size(); i++) {
+
+            if (nums[i] != 0) {
+
                 swap(nums[i], nums[j]);
+
+                // Move j to next zero
                 j++;
+
+                // while (j < nums.size() && nums[j] != 0) {
+                //     j++;
+                // }
             }
         }
     }
