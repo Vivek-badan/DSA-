cd7c399a0c5e3251fce3f4d9e000338bead983ec
Vivek Badan <vivek.badan@gmail.com>
2026-07-15 07:00:55 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (46.67%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -1,48 +1,55 @@
+// class Solution {
+// public:
+
+//     int ans(int &sumEven, int &sumOdd){
+
+//         while(sumEven > 0 && sumOdd > 0){
+
+//             if(sumEven > sumOdd){
+//                 sumEven = sumEven % sumOdd;
+//             }
+//             else if(sumOdd > sumEven){
+//                 sumOdd = sumOdd % sumEven;
+//             }
+//             else{
+//                 return sumEven;   
+//             }
+//         }
+
+//         return max(sumEven, sumOdd);
+//     }
+
+//     int gcdOfOddEvenSums(int n) {
+
+//         int cntEven = 1;
+//         int cntOdd = 1;
+
+//         int sumEven = 0;
+//         int sumOdd = 0;
+
+//         int i = 1;
+
+//         while(cntEven <= n || cntOdd <= n){
+
+//             if(i % 2 == 0 && cntEven <= n){
+//                 sumEven += i;
+//                 cntEven++;
+//             }
+//             else if(i % 2 != 0 && cntOdd <= n){
+//                 sumOdd += i;
+//                 cntOdd++;
+//             }
+
+//             i++;
+//         }
+
+//         return ans(sumEven, sumOdd);
+//     }
+// };
+
 class Solution {
 public:
-
-    int ans(int &sumEven, int &sumOdd){
-
-        while(sumEven > 0 && sumOdd > 0){
-
-            if(sumEven > sumOdd){
-                sumEven = sumEven % sumOdd;
-            }
-            else if(sumOdd > sumEven){
-                sumOdd = sumOdd % sumEven;
-            }
-            else{
-                return sumEven;   
-            }
-        }
-
-        return max(sumEven, sumOdd);
-    }
-
     int gcdOfOddEvenSums(int n) {
-
-        int cntEven = 1;
-        int cntOdd = 1;
-
-        int sumEven = 0;
-        int sumOdd = 0;
-
-        int i = 1;
-
-        while(cntEven <= n || cntOdd <= n){
-
-            if(i % 2 == 0 && cntEven <= n){
-                sumEven += i;
-                cntEven++;
-            }
-            else if(i % 2 != 0 && cntOdd <= n){
-                sumOdd += i;
-                cntOdd++;
-            }
-
-            i++;
-        }
-
-        return ans(sumEven, sumOdd);
+        return n;
     }
 };
