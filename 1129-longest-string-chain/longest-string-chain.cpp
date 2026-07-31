4c2d386c2cbfdc9b3d9db03035458e41445e28d3
Vivek Badan <vivek.badan@gmail.com>
2026-07-31 05:45:59 +0530

    Time: 33 ms (90.02%) | Memory: 18.8 MB (69.24%) - LeetSync

---
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+
+    static bool comp(string &s1 , string &s2){
+        return s1.size() < s2.size();
+    }
+    bool checkarr( string &s1 , string &s2){
+        if(s1.size()!=s2.size()+1) return false;
+
+        int first=0;
+        int second=0;
+
+        while(first<s1.size()){
+                if(s1[first]==s2[second]){
+                    first++;
+                    second++;
+                }
+                else {
+                    first++;
+                    }
+              }
+        if(first==s1.size() &&
+           second==s2.size()){
+            return true;
+           }
+        return false;
+    }
+    int longestStrChain(vector<string>& words) {
+
+        int n =words.size();
+        sort(begin(words),end(words),comp);
+        vector<int>dp(n,1);
+        int maxi=1;
+
+        for(int i =0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(checkarr(words[i],words[j]) && dp[j]+1>dp[i]){
+                    dp[i]=1+dp[j];
+                }
+            }
+            if(dp[i]>maxi){
+            maxi=dp[i];
+        }
+        }
+        
+
+        return maxi;
+
+ 
+        
+    }
+};
