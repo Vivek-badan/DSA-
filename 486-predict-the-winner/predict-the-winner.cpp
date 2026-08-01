92a75aa520ac250981bb2b45b89c8a78fb1c947a
Vivek Badan <vivek.badan@gmail.com>
2026-08-01 10:14:18 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (32.54%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-    int f(int i, int j, vector<int>& nums){
+    int f(int i, int j, vector<int>& nums,vector<vector<int>>&dp){
 
         if(i==j)
             return nums[i];
+        if(dp[i][j]!=-1) return dp[i][j];
 
-        int takeLeft = nums[i] - f(i+1,j,nums);
+        int takeLeft = nums[i] - f(i+1,j,nums,dp);
 
-        int takeRight = nums[j] - f(i,j-1,nums);
+        int takeRight = nums[j] - f(i,j-1,nums,dp);
 
-        return max(takeLeft,takeRight);
+        return dp[i][j]=max(takeLeft,takeRight);
     }
-    bool predictTheWinner(vector<int>& nums) {
-        return f(0,nums.size()-1,nums)>=0;
+    bool predictTheWinner(vector<int>& nums) {   vector<vector<int>> dp(nums.size(), vector<int>(nums.size(), -1));
+        return f(0,nums.size()-1,nums,dp)>=0;
     }
 };
