71b91017f874070a17d6fbf9b30ce6d44c64655d
Vivek Badan <vivek.badan@gmail.com>
2026-08-06 10:26:10 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (5.46%) - LeetSync

---
+++ 3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int productfunction(int n){
+        int product=1;
+        while(n>0){
+            int lastdigit=n%10;
+            product *=lastdigit;
+            n=n/10;
+        }
+        return product;
+    }
+    int smallestNumber(int n, int t) {
+        int ans=INT_MAX;
+        for(int i=n ; i<=108;i++){
+            if(productfunction(i) % t ==0){
+                return i;
+            }
+        }
+        return ans;
+        
+    }
+};
