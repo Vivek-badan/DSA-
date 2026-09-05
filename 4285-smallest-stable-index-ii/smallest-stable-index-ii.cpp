62e1d3095a82a8763a6c2c4ecd62a19e26d21899
Vivek Badan <vivek.badan@gmail.com>
2026-09-05 10:06:03 +0530

    Time: 7 ms (77.75%) | Memory: 202.5 MB (96.72%) - LeetSync

--- 4285-smallest-stable-index-ii/smallest-stable-index-ii.cpp
+++ 4285-smallest-stable-index-ii/smallest-stable-index-ii.cpp
@@ -6,14 +6,9 @@ public:
         vector<int>arr(n,-1);
 
         int mini=INT_MAX;
-        for(int i=n-1;i>=0;i--){
-            if(nums[i]<mini){
-                arr[i]=nums[i];
-                mini=nums[i];
-            }
-            else{
-                arr[i]=mini;
-            }
+        for (int i = n - 1; i >= 0; i--) {
+            mini = min(mini, nums[i]);
+            arr[i] = mini;
         }
         int maxi=INT_MIN;
         for(int i=0;i<n;i++){
